bb7612a76c89a51a8839e7af724163b50005323f
Manpreet Singh <ms20030904@gmail.com>
2025-02-19 16:00:12 +0530

    added logout functionality

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,7 @@
 const userModel=require('../models/user.models')
 
 const userService=require('../services/user.service')
+const blacklistTokenModel=require('../models/blacklistToken.models')
 const {validationResult}=require('express-validator')
 
 
@@ -48,4 +49,11 @@ module.exports.loginUser=async(req,res,next)=>{
 
 module.exports.getUserProfile=async(req,res,next)=>{
     res.status(200).json(req.user)//req.user is set in auth.middleware.js
+}
+module.exports.logoutUser=async(req,res,next)=>{
+    res.clearCookie('token')
+    const token=req.cookies.token||req.headers.authorization.split(' ')[1]
+    await blacklistTokenModel.create({token })
+
+    res.status(200).json({message:'Logged out successfully'})
 }
--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,10 +1,15 @@
 const userModel=require('../models/user.models')
 const bcrypt=require('bcrypt')
+const blacklistTokenModel=require('../models/blacklistToken.models')
 const jwt=require('jsonwebtoken')
 
 module.exports.authUser=async(req,res,next)=>{
-const token=req.cookies.token||req.headers.authorization.split(' ')[1]//split by ' 'and get at index[1]
+const token=req.cookies.token||req.headers.authorization?.split(' ')[1]//split by ' 'and get at index[1]
 if(!token){
+    return res.status(401).json({message:'Unauthorized'})
+}
+const isblacklisted=await blacklistTokenModel.findOne({token:token})
+if(isblacklisted){
     return res.status(401).json({message:'Unauthorized'})
 }
 try{
