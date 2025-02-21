4bbd790417aedeaf629ad1832f481896507b6136
Manpreet Singh <ms20030904@gmail.com>
2025-02-21 17:40:42 +0530

    Frontend pages initialised

---
+++ Frontend/frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Route,Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import UserLogin from './pages/UserLogin'
+import CaptainLogin from './pages/CaptainLogin'
+import UserSignup from './pages/UserSignup'
+import CaptainSignup from './pages/CaptainSignup'
+const App = () => {
+  return (
+    <div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<UserLogin/>}/>
+      <Route path="/signup" element={<UserSignup/>}/>
+      <Route path="/captain-signup" element={<CaptainSignup/>}/>
+      <Route path="/captain-login" element={<CaptainLogin/>}/>
+    </Routes> 
+    </div>
+  )
+}
+
+export default App
---
+++ Frontend/frontend/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <div className='h-screen pt-8 flex justify-between flex-col w-full bg-red-400'>
+            <img className='w-16 ml-8 ' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
+            <div className='bg-white pb-7 py-4 px-4 '>
+                <h2 className='text-3xl font-bold' >Get Started with Uber</h2>
+                <button className='w-full bg-black text-white rounded mt-5 py-3'>Continue</button>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Home
